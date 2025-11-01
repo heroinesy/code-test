a2e5775ea97d53f7812d57cc453b07abbcaa63dc
SY <101615063+heroinesy@users.noreply.github.com>
2025-11-01 22:26:45 +0900

    comment: DTO 주석 수정

--- src/main/java/com/wjc/codetest/product/model/request/CreateProductRequest.java
+++ src/main/java/com/wjc/codetest/product/model/request/CreateProductRequest.java
@@ -7,8 +7,17 @@ import lombok.Setter;
 
 /**
  * 상품 생성 요청 DTO
- * - Validation 추가 (@NotBlank)
- * - 기본 생성자(@NoArgsConstructor) 추가로 역직렬화 안정성 확보
+ *
+ * 1. 문제:
+ *    - DTO 필드(category, name)에 null/공백 문자열 요청이 그대로 통과함
+ *    - API 진입 전 입력 검증이 이뤄지지 않아 Controller-Service 단의 불필요한 예외 발생 가능
+ *
+ * 2. 원인:
+ *    -  - DTO 단에서 입력 유효성 검증 로직 부재
+ *
+ * 3. 개선안:
+ *    - @NotBlank 추가하여 문자열 필드에 대한 유효성 검증
+ *    - @NoArgsConstructor 추가로 역직렬화 안정성 확보
  */
 
 @Getter
